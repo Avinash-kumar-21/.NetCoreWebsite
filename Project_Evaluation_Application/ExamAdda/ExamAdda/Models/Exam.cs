a7b41aa00c6482bf51f106a0c9d0e7e35cc31313
Avinash-kumar-21 <Kumaravinash21june@gmail.com>
2024-08-27 19:35:36 +0530

    core web app and core web api

---
+++ Project_Evaluation_Application/ExamAdda/ExamAdda/Models/Exam.cs
@@ -0,0 +1,26 @@
+﻿namespace ExamAdda.Models
+{
+    public class Exam
+    {
+        public int ExamId { get; set; }
+
+        public DateTime? ExamDate { get; set; }
+
+        public string ExamName { get; set; }
+
+        public int? Score { get; set; }
+
+        public int Hours { get; set; }
+
+        public int? PassingMarks { get; set; }
+
+
+        public DateTime? CreatedDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int? ModifiedBy { get; set; }
+    }
+}
---
+++ Project_Evaluation_Application/ExamAdda/ExamAdda/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBAPICodeFirst.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Role is required")]
+        public string Role { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public int? CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public int? ModifiedBy { get; set; }
+        [DefaultValue(false)]
+        public bool? Active { get; set; }
+
+    }
+}
